8d34a2481e0aa18ae1279ca72d565946b62cee0c
Stuart Mumford <stuart@cadair.com>
2026-01-15 16:45:08 +0000

    TMP: Homepage

---
+++ conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'astropy.org'
+copyright = '2026, Astropy Community'
+author = 'Astropy Community'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx_design",
+]
+
+templates_path = ['_templates']
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    'LICENSE.rst',
+    'README.md',
+    'CONTRIBUTING.md',
+]
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'astropy'
+html_static_path = ['_static']
+
+html_theme_options = {
+    "show_prev_next": False,
+    "sst_is_root": True,
+}
+
+html_sidebars = {
+    "index": [],
+}
