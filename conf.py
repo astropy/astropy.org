e7e55f99de456d910d148d7273884373e2f8f635
Stuart Mumford <stuart@cadair.com>
2026-01-21 14:25:44 +0000

    Use theme in new home

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ rawfiles = ["annoucement_banner.html", "roles.json", "affiliated/registry.json"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'astropy'
+html_theme = 'astropy-unified'
 
 html_static_path = ['_static']
 
